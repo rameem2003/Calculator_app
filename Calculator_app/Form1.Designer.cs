cdbd83ce22e6ef2be91759d1d2bd38404d7c30d7
rameem2003 <rameem2019@gmail.com>
2021-12-18 21:20:50 +0600

    Name Added

--- Calculator_app/Form1.Designer.cs
+++ Calculator_app/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.button18 = new System.Windows.Forms.Button();
             this.button19 = new System.Windows.Forms.Button();
             this.calculator_clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // result_window
@@ -248,12 +249,23 @@
             this.calculator_clear.UseVisualStyleBackColor = false;
             this.calculator_clear.Click += new System.EventHandler(this.calculator_clear_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 18);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Designed by Mahmood Hassan Rameem";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(350, 367);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button13);
@@ -301,6 +313,7 @@
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.Button button19;
         private System.Windows.Forms.Button calculator_clear;
+        private System.Windows.Forms.Label label1;
     }
 }
 
